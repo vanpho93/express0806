a1513182106f319565e73d8c50b0bf7cb7feef89
Van Pho Nguyen <vanpho02@gmail.com>
2017-06-19 11:33:23 +0700

    ejs params

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ app.get('/', (req, res) => res.render('home'));
 
 app.get('/royal', (req, res) => res.render('royal'));
 
+app.get('/home', (req, res) => res.render('clothing'));
+
 app.get('/trangchu', (req, res) => res.send('Homepage'));
 
 app.get('/info/:id', require('./controllers/studentController'));
@@ -20,6 +22,11 @@ app.get('/info/:id', require('./controllers/studentController'));
 app.get('/show/:firstname/:lastname', require('./controllers/showController'));
 
 app.get('/tinh/:soA/:soB/:tenPhepTinh', require('./controllers/xuLyPhepTinh'));
+
+app.get('/a/:name/:fname', (req, res) => {
+    const { name, fname } = req.params;
+    res.render('a', { ten: name, ho: fname });
+});
 
 //https://github.com/vanpho93/oop0806b3
 
