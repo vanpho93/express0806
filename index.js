45f6a721dddfedd60c874c7a3e2346f3fa7113d7
Van Pho Nguyen <vanpho02@gmail.com>
2017-06-19 09:22:51 +0700

    params

--- index.js
+++ index.js
@@ -3,3 +3,17 @@ const express = require('express');
 const app = express();
 
 app.listen(3000);
+
+app.get('/', (req, res) => {
+    res.send('<h1>HELLO</h1>');
+});
+
+app.get('/trangchu', (req, res) => res.send('Homepage'));
+
+app.get('/info/:id', (req, res) => {
+    const idStudent = req.params.id;
+    res.send('Student info: ' + idStudent);
+});
+
+//route - router
+//1 route co the handle nhieu link
